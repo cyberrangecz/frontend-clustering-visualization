9f8cf4ce306d9334946a5b7f42b5a7a357948fa6
396296 <396296@mail.muni.cz>
2022-11-14 16:23:38 +0100

    Update app environment

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -2,50 +2,43 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const HOME_URL = 'https://localhost:4200'
+const HOME_URL = 'https://localhost:4200';
 
 export const environment = {
   production: false,
-  trainingServiceUrl: 'http://localhost:3000/kypo-rest-training/api/v1/',
-  // OIDC SETTINGS
-  // Url of the Identity Provider
-  issuer: 'https://oidc.muni.cz/oidc/',
-  // URL of the SPA to redirect the user after silent refresh
-  silentRefreshRedirectUri: HOME_URL,
-  // URL of the SPA to redirect the user to after login
-  redirectUri: HOME_URL,
-  // The SPA's id. The SPA is registered with this id at the config-server
-  clientId: 'b53f2660-8fa0-4d32-94e4-23a59d7e7077',
-  // set the scope for the permissions the client should request
-  scope: 'openid profile email',
-  sessionChecksEnabled: false,
+
+  statisticalVizConfig: {
+    trainingServiceUrl: 'http://localhost:3000/kypo-rest-training/api/v1/',
+  },
   authConfig: {
     guardMainPageRedirect: 'visualization',
     guardLoginPageRedirect: 'login',
-    interceptorAllowedUrls: [
-      'https://172.19.0.22',
-      'http://localhost',
-      'https://localhost'
-    ],
+    interceptorAllowedUrls: ['https://172.19.0.22', 'http://localhost', 'https://localhost'],
     authorizationStrategyConfig: {
-      authorizationUrl: 'https://172.19.0.22/kypo-rest-user-and-group/api/v1/users/info'
+      authorizationUrl: 'https://172.19.0.22/kypo-rest-user-and-group/api/v1/users/info',
     },
+    // OIDC SETTINGS
     providers: [
       {
         label: 'Login with MUNI',
         textColor: 'white',
         backgroundColor: '#002776',
         oidcConfig: {
-          issuer: 'https://172.19.0.22:8443/csirtmu-dummy-issuer-server/',
+          // Url of the Identity Provider
+          issuer: 'https://172.19.0.22:443/csirtmu-dummy-issuer-server/',
+          // The SPA's id. The SPA is registered with this id at the config-server
           clientId: '51d53826-3252-4db5-a4a1-51c51588efed',
+          // URL of the SPA to redirect the user after silent refresh
           redirectUri: HOME_URL,
+          // set the scope for the permissions the client should request
           scope: 'openid email profile',
           logoutUrl: 'https://172.19.0.22/csirtmu-dummy-issuer-server/endsession',
+          // URL of the SPA to redirect the user to after login
           postLogoutRedirectUri: HOME_URL,
-          clearHashAfterLogin: true
+          clearHashAfterLogin: true,
         },
       },
-    ]
+    ],
   },
 };
 
